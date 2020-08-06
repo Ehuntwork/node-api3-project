990356018e86de7f2bd127f2190b4e8c2663cbf1
emilme <emilme@sir.mle>
2020-08-06 11:05:08 -0700

    req.body returning undefined

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 
 const server = express();
+//LOCAL IMPORTS
+const userRouter = require('./users/userRouter')
+
+//GLOBAL MIDDLEWARE
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use('/users', userRouter)
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`A ${req.method} at ${req.url}. Time: ${Date.now()}`)
+  next()
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,101 @@
 const express = require('express');
-
+const user = require('./userDb')
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  // do your magic!
+  console.log(req.body)
+  user.insert(req.body)
+  .then(USER=>{
+    res.status(201).json(USER)
+  })
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json({
+      message: "Error posting new user"
+    })
+  })
 });
 
 router.post('/:id/posts', (req, res) => {
-  // do your magic!
+  user.insert(req.body)
+  .then(USER=>{
+    res.status(201).json(USER)
+  })
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json({
+      message: "Error posting new user"
+    })
+  })
 });
 
+//(SUCCESS)
 router.get('/', (req, res) => {
-  // do your magic!
+  user.get().then(USER=>{
+    res.status(200).json(USER)
+  })
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json({
+      message: "Error retrieving user data"
+    })
+  })
 });
 
+//(SUCCESS)
 router.get('/:id', (req, res) => {
-  // do your magic!
+  user.getById(req.params.id)
+  .then(USER=>{
+    res.status(200).json(USER)
+  })
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json({
+      message: "Error retrieving user data"
+    })
+  })
 });
 
+//(SUCCESS)
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  user.getUserPosts(req.params.id)
+  .then(USER=>{
+    res.status(200).json(USER)
+  })
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json({
+      message: "Error retrieving user data"
+    })
+  })
 });
 
+//(SUCCESS)
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  user.remove(req.params.id)
+  .then(()=>{
+    res.status(200).json({ message: 'The user has been deleted' })
+  })
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json({
+      message: "Error removing"
+    })
+  })
 });
+
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  user.update(req.params.id, req.body)
+  .then(USER=>{
+    res.status(200).json(USER)
+  })
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json({
+      message: "Error updating"
+    })
+  })
 });
 
 //custom middleware
